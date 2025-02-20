e53a743a722c452577d56e54a1c165ac9a7e59e9
Kyle Zhou <kylezhou2222@gmail.com>
2025-02-19 22:43:02 -0800

    bring module name into compliance

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_zhouzhouthezhou_adder (
+module tt_um_zhouzhouthezhou_adder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
